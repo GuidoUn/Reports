43d0257361f0ae156c7a19f032aa94892049d118
GuidoUn <40839146+GuidoUn@users.noreply.github.com>
2019-06-10 17:03:30 -0300

    Update App.js

--- PruebaGuido/App.js
+++ PruebaGuido/App.js
@@ -160,7 +160,7 @@ const styles = StyleSheet.create({
   title:{
     textAlign:'center',
     fontSize: 22,
-    marginBottom: 30
+    marginBottom: 15
   },
   button:{
     backgroundColor: 'red',
